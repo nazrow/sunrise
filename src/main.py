8cafb62578bed1ab0da350d1de4b65b8891c1b0b
nazrow <nazrow@me.com>
2024-02-27 21:45:51 +0300

    lets try troykahat directly

--- src/main.py
+++ src/main.py
@@ -1,19 +1,22 @@
 import troykahat
 import time
 import datetime
-from gpiozero import Servo
+# from gpiozero import Servo
 
 servomin = 0.000544
 servomax = 0.0024
 step = (servomax - servomin) / 200
 now = (servomax + servomin) / 2
-servo = Servo("WPI24", min_pulse_width=servomin, max_pulse_width=servomax)
+gpio = troykahat.GpioExpander()
+gpio.pinMode(0, gpio.OUTPUT)
+# servo = Servo("WPI24", min_pulse_width=servomin, max_pulse_width=servomax)
 
 print(f'Hey bitch! It\'s {datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%s")}')
 while True:
     print(now)
     time.sleep(0.1)
-    servo.value = now
+    # servo.value = now
+    gpio.analogWrite(0, now)
     now = now + step
     if now > servomax:
         step = step * (-1)
