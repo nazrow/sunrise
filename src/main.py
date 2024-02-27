f45adf1f9626b1c7bbfb2af31d7eb4acd92deea1
nazrow <nazrow@me.com>
2024-02-27 23:30:09 +0300

    aaand fuck troykahat

--- src/main.py
+++ src/main.py
@@ -1,22 +1,18 @@
-import troykahat
 import time
 import datetime
-# from gpiozero import Servo
+from gpiozero import Servo
 
 servomin = 0.000544
 servomax = 0.0024
 step = (servomax - servomin) / 200
 now = (servomax + servomin) / 2
-gpio = troykahat.analog_io()
-gpio.pinMode(0, gpio.OUTPUT)
-# servo = Servo("WPI24", min_pulse_width=servomin, max_pulse_width=servomax)
+servo = Servo("WPI24", min_pulse_width=servomin, max_pulse_width=servomax)
 
 print(f'Hey bitch! It\'s {datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%s")}')
 while True:
     print(now)
     time.sleep(0.1)
-    # servo.value = now
-    gpio.analogWrite(0, now)
+    servo.value = now
     now = now + step
     if now > servomax:
         step = step * (-1)
