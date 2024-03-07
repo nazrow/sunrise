e98a4e0bc4b3c1a625b8e1066741f0c78c25d8ae
nazrow <nazrow@me.com>
2024-03-08 00:09:54 +0300

    pigpio as default

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 from time import sleep
 from gpiozero import Servo
 from gpiozero.pins.pigpio import PiGPIOFactory
-import random
 
 pigpio = PiGPIOFactory()
 servo = Servo("WPI24", pin_factory=pigpio)
-while True:
-    servo.value = random.uniform(-1, 1)
-    sleep(random.uniform(0.01, 0.5))
+servo.value = 1
+sleep(10)
+servo.value = -1
+sleep(10)
+servo.close()
