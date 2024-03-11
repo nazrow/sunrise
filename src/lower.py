d9775c24c519463f6b7a4e3f83b43335289eb4a4
Влад Савенков <vmsavenkov@sberbank.ru>
2024-03-11 21:11:35 +0300

    calibration

--- src/lower.py
+++
@@ -1,9 +0,0 @@
-from time import sleep
-from gpiozero import Servo
-from gpiozero.pins.pigpio import PiGPIOFactory
-
-pigpio = PiGPIOFactory()
-servo = Servo("WPI24", pin_factory=pigpio)
-servo.value = -1
-sleep(8)
-servo.close()
--- src/main.py
+++ src/main.py
@@ -1,7 +1,15 @@
 from time import sleep
 from gpiozero import Servo
 from gpiozero.pins.pigpio import PiGPIOFactory
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument('-s', '--speed')
+parser.add_argument('-t', '--time')
+args = parser.parse_args()
+speed = float(args.speed)
+time = float(args.time) / 1000
 pigpio = PiGPIOFactory()
 servo = Servo("WPI24", pin_factory=pigpio)
+
 servo.close()
